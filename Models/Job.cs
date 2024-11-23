a44219cfa5bcc3aa15a96ec15cabc152e34b0f8d
qinscode <fudong.qin@outlook.com>
2024-11-23 23:36:49 +0800

    feat(Job): add salary range and location type fields
    - Add MinSalary and MaxSalary properties to represent salary range- Add LocationType property to specify job location
    - Update Job model to include new fields

--- Models/Job.cs
+++ Models/Job.cs
@@ -11,6 +11,9 @@ public class Job
     public string? WorkType { get; set; }
     public string? JobType { get; set; }
     public string? PayRange { get; set; }
+    public int? MinSalary { get; set; }
+    public int? MaxSalary { get; set; }
+    public string? LocationType { get; set; }
     public string? Suburb { get; set; }
     public string? Area { get; set; }
     public string? Url { get; set; }
